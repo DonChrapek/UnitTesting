d44850df7771bceb362a89fd1b64233539c95414
DonChrapek <daniel.chrapczynski@gmail.com>
2017-11-03 21:16:26 +0100

    Completed tests for Creditor Class

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -6,7 +6,32 @@ import static org.junit.Assert.*;
 
 public class CreditorTest {
     @Test
-    public void refund() throws Exception {
+    public void testAddingFundsIncrementsAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        assertEquals(50, creditor.getAvailableFunds());
     }
 
+    @Test
+    public void testRefundingReturnsAllAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(10);
+
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
+    }
+
+    @Test
+    public void refundingResetsAvailableFundsToZero() throws Exception {
+        Creditor creditor = new Creditor();
+        creditor.addFunds(300);
+        creditor.refund();
+
+        assertEquals(0, creditor.getAvailableFunds());
+    }
 }
