70fe6fc293aa5d25ca901d3278d18456080a1c7f
DonChrapek <daniel.chrapczynski@gmail.com>
2017-11-03 21:38:39 +0100

    Added fixture to CreditorTest

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -1,14 +1,21 @@
 package com.teamtreehouse.vending;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CreditorTest {
+
+    private Creditor creditor;
+
+    @Before
+    public void setUp() throws Exception {
+        creditor = new Creditor();
+    }
+
     @Test
     public void testAddingFundsIncrementsAvailableFunds() throws Exception {
-        Creditor creditor = new Creditor();
-
         creditor.addFunds(25);
         creditor.addFunds(25);
 
@@ -17,8 +24,6 @@ public class CreditorTest {
 
     @Test
     public void testRefundingReturnsAllAvailableFunds() throws Exception {
-        Creditor creditor = new Creditor();
-
         creditor.addFunds(10);
 
         int refund = creditor.refund();
@@ -28,7 +33,6 @@ public class CreditorTest {
 
     @Test
     public void refundingResetsAvailableFundsToZero() throws Exception {
-        Creditor creditor = new Creditor();
         creditor.addFunds(300);
         creditor.refund();
 
