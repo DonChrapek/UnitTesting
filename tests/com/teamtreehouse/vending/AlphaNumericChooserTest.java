378c9cc3747d5900fd19b8aac88ce41e422271f7
DonChrapek <daniel.chrapczynski@gmail.com>
2017-11-03 22:54:28 +0100

    Added exceptions to AlphaNumericChooserTest

--- tests/com/teamtreehouse/vending/AlphaNumericChooserTest.java
+++ tests/com/teamtreehouse/vending/AlphaNumericChooserTest.java
@@ -1,5 +1,34 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AlphaNumericChooserTest {
 
+    private AlphaNumericChooser chooser;
+
+    @Before
+    public void setUp() throws Exception {
+        chooser = new AlphaNumericChooser(26, 10);
+    }
+
+    @Test
+    public void validInputReturnsProperLocation() throws Exception {
+        AlphaNumericChooser.Location loc = chooser.locationFromInput("B4");
+
+        assertEquals("proper row",1, loc.getRow());
+        assertEquals("proper column",3, loc.getColumn());
+    }
+
+    @Test(expected = InvalidLocationException.class)
+    public void choosingWrongInputIsNotAllowed() throws Exception {
+        chooser.locationFromInput("WRONG");
+    }
+
+    @Test(expected = InvalidLocationException.class)
+    public void choosingLargerThanMaxIsNotAllowed() throws Exception {
+        chooser.locationFromInput("B52");
+    }
 }
